2a7cec9e97b92259862a93797c02a0fc8b4eefdf
Loan Laux <loan@outgrow.io>
2020-05-07 11:23:22 +0200

    feat: add migrations for Admin UI
    Signed-off-by: Loan Laux <loan@outgrow.io>

--- migrator.config.js
+++ migrator.config.js
@@ -1,6 +1,11 @@
 export default {
   tracks: [
     {
+      namespace: "admin-ui",
+      path: "../reaction-3/@reactioncommerce/plugin-admin-ui/index.js",
+      version: 2
+    },
+    {
       namespace: "simple-authorization",
       package: "@reactioncommerce/plugin-simple-authorization",
       version: 4
