541a575406dabc41bb6d124d367b2365296950e9
Todd Hiles <todd@bugslifesolutions.com>
2023-12-16 01:29:06 +0000

    fix: use @bugslifesolutions and latest migration versions

--- migrator.config.js
+++ migrator.config.js
@@ -2,23 +2,33 @@ export default {
   tracks: [
     {
       namespace: "accounts",
-      package: "@reactioncommerce/api-plugin-accounts",
+      package: "@bugslifesolutions/api-plugin-accounts",
       version: 3
     },
     {
       namespace: "simple-authorization",
-      package: "@reactioncommerce/api-plugin-authorization-simple",
-      version: 5
+      package: "@bugslifesolutions/api-plugin-authorization-simple",
+      version: 6
     },
     {
       namespace: "tags",
-      package: "@reactioncommerce/api-plugin-tags",
+      package: "@bugslifesolutions/api-plugin-tags",
       version: 2
     },
     {
       namespace: "email",
-      package: "@reactioncommerce/api-plugin-email",
+      package: "@bugslifesolutions/api-plugin-email",
       version: 2
+    },
+    {
+      namesapce: "promotion-coupons",
+      package: "@bugslifesolutions/api-plugin-promotions-coupons",
+      version: 2
+    },
+    {
+      namesapce: "nodemailer",
+      package: "@bugslifesolutions/bl-api-plugin-nodemailer",
+      version: 1
     }
   ]
 };
